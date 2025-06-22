f1b878e119a3db9853de749bd804d3986f819a15
TLabAltoh <tlabaltoh@gmail.com>
2025-06-22 13:33:30 +0900

    Fix bounding box gizmo panel

--- marching_cube.py
+++ marching_cube.py
@@ -379,7 +379,6 @@ class SDF2MESH_PT_Generate(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = "SDF2Mesh"
-    bl_context = "objectmode"
 
     def draw(self, context):
         sc = context.scene
@@ -485,17 +484,3 @@ def unregister():
     
     for c in classes:
         bpy.utils.unregister_class(c)
-        
-"""
-coords = [(1, 1, 1), (-2, 0, 0), (-2, -1, 3), (0, 1, 1)]
-shader = gpu.shader.from_builtin('UNIFORM_COLOR')
-batch = batch_for_shader(shader, 'LINES', {"pos": coords})
-
-
-def draw():
-    shader.uniform_float("color", (1, 1, 0, 1))
-    batch.draw(shader)
-
-
-bpy.types.SpaceView3D.draw_handler_add(draw, (), 'WINDOW', 'POST_VIEW')
-"""
